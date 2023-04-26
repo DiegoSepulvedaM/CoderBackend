c86f2027ee3ca75488adbf086087613b8fcfcb60
DiegoSepulvedaM <diego.sepu@outlook.es>
2023-04-26 17:30:33 -0400

    hs

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+import {
+    Server
+} from 'socket.io'
+import productsRouter from './routes/api/products.router.js'
+import viewsRouter from './routes/web/views.router.js'
+
+const app = express()
+app.use(express.static(`${__dirname}/public`))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'handlebars')
+
+app.use('/realtimeproducts', viewsRouter)
+app.use('/api/products', productsRouter);
+
+
+const server = app.listen(8080, () => console.log('Listening server on port 8080'))
+
+const io = new Server(server)
+
+app.set('socketio', io);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+  
+    const products = // obtener la lista actual de productos
+    socket.emit('updateProducts', productsRouter);
+  });
